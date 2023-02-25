caae49f21d44531f5425c7af906b0d7d4cb0619f
tallapallesandhya <sandhyatallapalli32@gmail.com>
2023-02-25 11:05:43 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from app.models import *
+from django.views.generic import ListView,TemplateView,DetailView
+
+class Home(TemplateView):
+    template_name='app/home.html'
+
+class SchoolList(ListView):
+    model=School
+    context_object_name='schools'
+    #template_name='app/school_list.html'
+    #queryset=School.objects.filter(name='Jspiders')
+    ordering=['name']
+
+class SchoolDetail(DetailView):
+    model=School
+    context_object_name='sc'
+
